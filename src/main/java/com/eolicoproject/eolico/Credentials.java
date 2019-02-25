94da2b9b867e82ca0438ca6bec034a0d91ee7b9e
Rute <rutesabreu@gmail.com>
2019-02-25 00:38:59 -0300

    Login
    Implementacao inicial do login

---
+++ src/main/java/com/eolicoproject/eolico/Credentials.java
@@ -0,0 +1,27 @@
+package com.eolicoproject.eolico;
+
+public class Credentials {
+
+    private String nome;
+    private String senha;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+
+
+}
---
+++ src/main/java/com/eolicoproject/eolico/JWTLoginFilter.java
@@ -0,0 +1,51 @@
+package com.eolicoproject.eolico;
+import java.io.IOException;
+import java.util.Collections;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
+
+    protected JWTLoginFilter(String url, AuthenticationManager authManager) {
+        super(new AntPathRequestMatcher(url));
+        setAuthenticationManager(authManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException, IOException, ServletException {
+
+        Credentials credentials = new ObjectMapper()
+                .readValue(request.getInputStream(), Credentials.class);
+
+        return getAuthenticationManager().authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        credentials.getNome(),
+                        credentials.getSenha(),
+                        Collections.emptyList()
+                )
+        );
+    }
+
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain,
+            Authentication auth) throws IOException, ServletException {
+
+        TokenAuthenticationService.addAuthentication(response, auth.getName());
+    }
+}
